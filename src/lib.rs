b24fdbaadd247cfbd2998b1d08d61b95870d98c1
pndyjack <pndyjack@gmail.com>
2019-02-27 13:49:31 -0500

    Implemented a recursive algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,64 @@
-pub fn nth(n: u32) -> Option<u32> {
+// for tail recursion support
+#[macro_use]
+extern crate tramp;
+use tramp::{tramp, Rec};
+
+pub fn nth(n: u32) -> u32 {
   if n == 0 {
-    return None;
+    return 2;
   }
-  let mut prime_count = 0;
-  for number in 1..u32::max_value() {
-    if check_if_prime(number) {
-      prime_count += 1;
-    }
-    if prime_count == n {
-      return Some(number);
-    }
-  }
-  None
+  find_prime(n)
 }
 
-pub fn check_if_prime(n: u32) -> bool {
+pub fn find_prime(n: u32) -> u32 {
+  const FIRST_PRIME: u32 = 3;
+  const INIT_COUNT: i32 = 1;
+  const INIT_NUMBER: u32 = 2;
+
+  fn until_prime(next_number: u32, count: i32, until: u32, last_prime: u32) -> Rec<u32> {
+    if count == until as i32 {
+      rec_ret!(last_prime)
+    }
+
+    let (local_count, local_last_prime) = if is_prime(next_number) {
+      (count + 1, next_number)
+    } else {
+      (count, last_prime)
+    };
+
+    rec_call!(until_prime(
+      next_number + 1,
+      local_count,
+      until,
+      local_last_prime
+    ))
+  }
+  tramp(until_prime(INIT_NUMBER, INIT_COUNT, n, FIRST_PRIME))
+}
+
+pub fn is_prime(n: u32) -> bool {
   if n <= 1 {
     return false;
   }
+
   if n <= 3 {
     return true;
   }
+
   if n % 2 == 0 || n % 3 == 0 {
     return false;
   }
-  let mut i = 5;
-  while i * i <= n {
+
+  fn is_prime_rec(n: u32, i: u32) -> Rec<bool> {
     if n % i == 0 || n % (i + 2) == 0 {
-      return false;
+      rec_ret!(false);
     }
-    i += 6;
+
+    if i * i <= n {
+      rec_call!(is_prime_rec(n, i + 6));
+    } else {
+      rec_ret!(true);
+    }
   }
-  true
+  tramp(is_prime_rec(n, 5))
 }
